f23d41d59a5c2c67a999f8ec5fae1911a15146f2
JosJuice <josjuice@gmail.com>
2014-03-20 14:19:01 +0100

    Find all files in a folder

--- src/se/zeldaforumet/josjuice/punparse/PunParse.java
+++ src/se/zeldaforumet/josjuice/punparse/PunParse.java
@@ -1,15 +1,23 @@
 package se.zeldaforumet.josjuice.punparse;
 
+import java.io.File;
+
 /**
  * @author Jos
  */
 public class PunParse {
+    
+    public static void parseFolder(String pathname) {
+        File folder = new File(pathname);
+        File[] files = folder.listFiles();
+    }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO: error handling
+        parseFolder(args[0]);
     }
 
 }
