251218fd238e34da695812f51150135aa55cf353
JosJuice <josjuice@gmail.com>
2014-03-23 10:22:38 +0100

    Add a basic HTML file parse loop

--- src/se/zeldaforumet/josjuice/punparse/PunParse.java
+++ src/se/zeldaforumet/josjuice/punparse/PunParse.java
@@ -1,6 +1,10 @@
 package se.zeldaforumet.josjuice.punparse;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
 import java.io.File;
+import java.io.IOException;
 
 /**
  * @author Jos
@@ -10,6 +14,23 @@ public class PunParse {
     public static void parseFolder(String pathname) {
         File folder = new File(pathname);
         File[] files = folder.listFiles();
+        if (files != null) {
+            for (int i = 0; i < files.length; i++) {
+                Document document;
+                try {
+                    document = Jsoup.parse(files[i], null);
+                    System.out.println(document);
+                    System.out.println("Processed file " +
+                                       (i + 1) + "/" + files.length);
+                } catch (IOException e) {
+                    System.out.println("Could not read file " +
+                                       (i + 1) + "/" + files.length +
+                                       files[i].getName());
+                }
+            }
+        } else {
+            System.out.println("No files were found.");
+        }
     }
 
     /**
