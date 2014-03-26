0042b251fb68dd030a0616d5c7c13e8996214a67
JosJuice <josjuice@gmail.com>
2014-03-26 16:55:42 +0100

    Removed the TODO since it's handled by the issue tracker now

--- src/se/zeldaforumet/josjuice/punparse/PunParse.java
+++ src/se/zeldaforumet/josjuice/punparse/PunParse.java
@@ -15,7 +15,6 @@ public class PunParse {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO: error handling
         parseFolder(args[0]);
     }
     
