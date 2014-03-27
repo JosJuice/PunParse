3d481f5581947fa3c17ca323d508290ebc31de2a
JosJuice <josjuice@gmail.com>
2014-03-27 10:07:53 +0100

    Parse subdirectories (fixes issue #2)

--- src/se/zeldaforumet/josjuice/punparse/PunParse.java
+++ src/se/zeldaforumet/josjuice/punparse/PunParse.java
@@ -15,28 +15,35 @@ public class PunParse {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        parseFolder(args[0]);
+        File directory = new File(args[0]);
+        parseDirectory(directory);
     }
     
     /**
-     * Parses every file in a folder.
-     * @param pathname pathname of folder
+     * Parses every file in a directory.
+     * @param directory directory containing files to parse
      */
-    public static void parseFolder(String pathname) {
-        File folder = new File(pathname);
-        File[] files = folder.listFiles();
+    public static void parseDirectory(File directory) {
+        File[] files = directory.listFiles();
         if (files != null) {
             for (int i = 0; i < files.length; i++) {
-                try {
-                    Document document = Jsoup.parse(files[i], null);
-                    parseDocument(document);
-                    System.out.println("Processed file " +
-                                       (i + 1) + "/" + files.length + ": " +
-                                       files[i].getName());
-                } catch (IOException e) {
-                    System.err.println("Could not read file " +
-                                       (i + 1) + "/" + files.length + ": " +
-                                       files[i].getName());
+                if (files[i].isDirectory()) {
+                    // Parse subdirectories recursively
+                    System.out.println("Entering subdirectory...");
+                    parseDirectory(files[i]);
+                    System.out.println("Exiting subdirectory...");
+                } else {
+                    try {
+                        Document document = Jsoup.parse(files[i], null);
+                        parseDocument(document);
+                        System.out.println("Processed file " +
+                                           (i + 1) + "/" + files.length + ": " +
+                                           files[i].getName());
+                    } catch (IOException e) {
+                        System.err.println("Could not read file " +
+                                           (i + 1) + "/" + files.length + ": " +
+                                           files[i].getName());
+                    }
                 }
             }
         } else {
