217e90381b80f3787eeb346f76396a3fa23dbe14
JosJuice <josjuice@gmail.com>
2014-03-28 11:04:53 +0100

    Add private variables for Post

--- src/se/zeldaforumet/josjuice/punparse/data/Post.java
+++ src/se/zeldaforumet/josjuice/punparse/data/Post.java
@@ -10,6 +10,44 @@ public class Post {
     public static final int UNKNOWN_TOPIC_ID = 0;
     
     /**
+     * Post ID
+     */
+    private int id;
+    /**
+     * Username of the user that made the post
+     */
+    private String poster;
+    /**
+     * ID of the user that made the post
+     */
+    private int posterId;
+    /**
+     * The content of the post
+     */
+    private String message;
+    /**
+     * If true, text will never be converted to graphical smilies
+     * when the post is displayed
+     */
+    private boolean hideSmilies;
+    /**
+     * Date posted (Unix timestamp)
+     */
+    private int posted;
+    /**
+     * Date last edited (Unix timestamp)
+     */
+    private int edited;
+    /**
+     * Username of the user that edited the post last
+     */
+    private String editedBy;
+    /**
+     * ID of the topic containing this post
+     */
+    private int topicId;
+    
+    /**
      * Constructs a Post.
      * @param postElement HTML element representing a post (.blockpost)
      * @param topicId ID of the topic containing this post
