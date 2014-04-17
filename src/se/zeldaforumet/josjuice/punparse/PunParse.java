7d0718cbd8ae09671cd65eacacae0c8af8ab7388
JosJuice <josjuice@gmail.com>
2014-04-17 14:23:01 +0200

    Add "Connecting to SQL database..." printout

--- src/se/zeldaforumet/josjuice/punparse/PunParse.java
+++ src/se/zeldaforumet/josjuice/punparse/PunParse.java
@@ -20,6 +20,7 @@ public class PunParse {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("Connecting to SQL database...");
         try (Database database = new Database(args[1])) {
             File directory = new File(args[0]);
             parseDirectory(directory, database);
