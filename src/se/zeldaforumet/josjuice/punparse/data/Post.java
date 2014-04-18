d47b43dfe6e06a25f0ba8aa041bbbf4b06f74162
JosJuice <josjuice@gmail.com>
2014-04-18 19:29:07 +0200

    Parse posts

--- src/se/zeldaforumet/josjuice/punparse/data/Post.java
+++ src/se/zeldaforumet/josjuice/punparse/data/Post.java
@@ -8,6 +8,8 @@ import org.jsoup.nodes.Element;
 public class Post {
     
     public static final int UNKNOWN_TOPIC_ID = 0;
+    public static final int UNKNOWN_DATE = 0;
+    private static final String POSTER_ID_DELIMITER = "id=";
     
     /**
      * Post ID
@@ -41,12 +43,12 @@ public class Post {
     private int posted;
     
     /**
-     * Date last edited (Unix timestamp)
+     * Date last edited (Unix timestamp), or UNKNOWN_DATE if not edited
      */
     private int edited;
     
     /**
-     * Username of the user that edited the post last
+     * Username of the user that last edited the post, or null if not edited
      */
     private String editedBy;
     
@@ -57,12 +59,94 @@ public class Post {
     
     /**
      * Constructs a <code>Post</code>.
-     * @param postElement HTML element representing a post.
+     * @param element HTML element representing a post.
      * The element should always have the <code>.blockpost</code> class.
      * @param topicId ID of the topic containing this post.
      * Use <code>UNKNOWN_TOPIC_ID</code> if unknown.
+     * @throws NullPointerException if a required child element does not exist
+     * @throws IllegalArgumentException if some required data is invalid
      */
-    public Post(Element postElement, int topicId) {
-        // TODO
+    public Post(Element element, int topicId) throws NullPointerException,
+                                                     IllegalArgumentException {
+        // Find post ID
+        String id = element.id();
+        try {
+            // Skip the first character 'p' and use the rest of the string as ID
+            this.id = Integer.parseInt(id.substring(1));
+        } catch (NumberFormatException | IndexOutOfBoundsException e) {
+            throw new IllegalArgumentException("Invalid post ID: " + id);
+        }
+        
+        // Find poster username and ID
+        Element posterElement = element.getElementsByTag("dt").first();
+        // Get poster username
+        poster = posterElement.text();
+        // Get URL to poster's profile
+        String posterUrl = posterElement.getElementsByAttribute("href").
+                                          first().attributes().get("href");
+        // Get poster ID from profile URL
+        // (this will fail if id isn't the last parameter in the URL)
+        try {
+            posterId = Integer.parseInt(posterUrl.substring(posterUrl.
+                                        lastIndexOf(POSTER_ID_DELIMITER) +
+                                        POSTER_ID_DELIMITER.length()));
+        } catch (NumberFormatException | IndexOutOfBoundsException e) {
+            throw new IllegalArgumentException("Could not determine poster ID" +
+                                               "for post #" + this.id);
+        }
+        
+        // Find message text
+        // TODO get BBCode, not just plaintext
+        message = element.getElementsByClass("postmsg").first().text();
+        
+        // TODO check if smilies need to be displayed
+        hideSmilies = true;
+        
+        // TODO find date posted
+        posted = UNKNOWN_DATE;
+        
+        // TODO find out if edited
+        edited = UNKNOWN_DATE;
+        editedBy = null;
+        
+        // Find topic ID (well, there's not much finding going on, really :))
+        this.topicId = topicId;
     }
+    
+    public int getId() {
+        return id;
+    }
+    
+    public String getPoster() {
+        return poster;
+    }
+    
+    public int getPosterId() {
+        return posterId;
+    }
+    
+    public String getMessage() {
+        return message;
+    }
+    
+    public boolean getHideSmilies() {
+        return hideSmilies;
+    }
+    
+    public int getPosted() {
+        return posted;
+    }
+    
+    public int getEdited() {
+        return edited;
+    }
+    
+    public String getEditedBy() {
+        return editedBy;
+    }
+    
+    public int getTopicId() {
+        return topicId;
+    }
+    
 }
