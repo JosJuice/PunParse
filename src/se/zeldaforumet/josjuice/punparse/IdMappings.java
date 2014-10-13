e00798f1ba69c1a645c4d6dbe0fc89deb3b4d346
JosJuice <josjuice@gmail.com>
2014-10-13 18:51:40 +0200

    Make associating posts with topics work regardless of input order

--- src/se/zeldaforumet/josjuice/punparse/IdMappings.java
+++ src/se/zeldaforumet/josjuice/punparse/IdMappings.java
@@ -1,5 +1,6 @@
 package se.zeldaforumet.josjuice.punparse;
 
+import java.sql.SQLException;
 import java.util.List;
 import java.util.TreeMap;
 
@@ -10,14 +11,28 @@ import java.util.TreeMap;
  */
 public class IdMappings {
     
+    /*
+     * Maps topic IDs to posts.
+     * Key: Post ID
+     * Value: Topic ID
+     */
     private final TreeMap<Integer, Integer> postTopicMap = new TreeMap<>();
+    
+    /*
+     * Stores posts so that they can be sent to a database later when the
+     * topic ID is found. To make lookups easy, each page of posts is present
+     * one time for each post ID. (A page with 15 posts has 15 key-value pairs)
+     * Key: Post ID
+     * Value: Pages of posts
+     */
+    private final TreeMap<Integer, List<Post>> postQueue = new TreeMap<>();
     
     /**
      * Attempts to get the topic ID of a page of posts. This will only work if a
      * mapping has been added for the topic using
      * {@link #setTopicId(Topic) setTopicId}. If that is not the case,
      * {@code null} will be returned and the posts will be queued in this object
-     * and sent to the database once a matching mapping is added.
+     * and sent to the database later once a matching mapping is added.
      * 
      * To clarify: If {@code null} is returned, this object will handle sending
      * the posts to the database. If any other value is returned, it will not.
@@ -34,7 +49,10 @@ public class IdMappings {
                     return possibleId;
                 }
             }
-            // TODO handle the case of needing to queue posts
+            // If this is reached, the ID wasn't found. The posts will be queued
+            for (Post post : posts) {
+                postQueue.put(post.getId(), posts);
+            }
             return null;
         }
     }
@@ -49,9 +67,31 @@ public class IdMappings {
      * @param ui A {@link UserInterface} for error display, or {@code null}.
      */
     public void setTopicId(Topic topic, Database database, UserInterface ui) {
+        List<Post> queuedPosts;
         synchronized (postTopicMap) {
             postTopicMap.put(topic.getLastPostId(), topic.getId());
-            // TODO handle the case of queued posts matching this topic
+            
+            // Check if there any queued posts match, and prepare for submitting
+            queuedPosts = postQueue.get(topic.getLastPostId());
+            if (queuedPosts != null) {
+                for (Post post : queuedPosts) {
+                    postQueue.remove(post.getId());
+                }
+            }
+        }
+        
+        // Submit matching queued posts if there are any
+        // This is done outside the synchronized block so that other threads can
+        // use the postTopicMap while this thread only is using the database
+        if (queuedPosts != null) {
+            for (Post post : queuedPosts) {
+                try {
+                    database.insert(post, topic.getId());
+                } catch (SQLException e) {
+                    System.err.println("SQL error when submitting queued post #"
+                            + post.getId() + ": " + e.getLocalizedMessage());
+                }
+            }
         }
     }
     
